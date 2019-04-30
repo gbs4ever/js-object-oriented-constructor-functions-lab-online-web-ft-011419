de66c9055d8b0a6159062d3d24abd4682dce761c
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-30 05:43:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function Scooter(year,color,model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+Scooter with year, color, and model properties
+Driver with name, age, and experience properties
+PickupLocation with address and city properties
