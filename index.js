c55ca1c7a4ea9c657929c6968411aa0829b12bab
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-30 05:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function Scooter(year,color,model){
   this.color = color
   this.model = model
 }
-function (name,age,experience){
+function Driver(name,age,experience){
   this.name = name
   this.age = age
   this.experience = experience
